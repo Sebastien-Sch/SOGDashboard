3215d6ec14998acd8f7d017898a6edcf35c3503f
Ophelie Arnoult <ophelie.arnoult250@gmail.com>
2025-11-25 22:11:08 +0100

    data chartjs topfab

--- src/app/chart/ranking-fab-chart/ranking-fab-chart.ts
+++ src/app/chart/ranking-fab-chart/ranking-fab-chart.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../services/api-service';
 
 @Component({
   selector: 'app-ranking-fab-chart',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ranking-fab-chart.html',
   styleUrl: './ranking-fab-chart.scss',
 })
 export class RankingFabChart {
-
+  constructor(private api: ApiService) { }
+  fabIds: string[] = [];
+  fabValues: number[] = [];
+  rankingData: { fabId: string; nbVentes: number }[] = [];
+  ngOnInit() {
+    this.api.getData().subscribe({
+      next: (res) => {
+        this.rankingData = res.topFabricants.data.map((nbVentes: number, index: number) => ({
+          fabId: res.topFabricants.labels[index],
+          nbVentes: nbVentes
+        }));
+        this.fabIds = res.topFabricants.labels;
+        this.fabValues = res.topFabricants.data;
+      },
+      error: (err) => {
+        console.error('Erreur API chart:', err);
+      }
+    });
+  }
 }
