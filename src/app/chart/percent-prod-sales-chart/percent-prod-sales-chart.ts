e44dea45a06922b2597db3d29c21427d6783934a
Ophelie Arnoult <ophelie.arnoult250@gmail.com>
2025-11-26 09:59:56 +0100

    data chartjs percentSales

--- src/app/chart/percent-prod-sales-chart/percent-prod-sales-chart.ts
+++ src/app/chart/percent-prod-sales-chart/percent-prod-sales-chart.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
+import { ApiService } from '../../services/api-service';
 Chart.register(...registerables);
 
 @Component({
@@ -12,39 +13,52 @@ Chart.register(...registerables);
 export class PercentProdSalesChart implements AfterViewInit {
   @ViewChild('chartCanvas', { static: true }) chartCanvas!: ElementRef<HTMLCanvasElement>;
 
+  private chartInstance: Chart | null = null;
+  constructor(private api: ApiService) { }
+
   ngAfterViewInit() {
+
     const ctx = this.chartCanvas.nativeElement.getContext('2d');
     if (ctx) {
       // Initialize your chart here
-      const labels = ['Pourcentage de production', 'Pourcentage de produits en vente'];
-      const data = [350, 200];
-      new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-          labels: labels,
-          datasets: [{
-            data: data,
-            backgroundColor: [
-              'rgba(75, 192, 192, 0.5)',
-              'rgba(54, 162, 235, 0.5)',
-            ],
-            borderColor: [
-              'rgba(75, 192, 192, 1)',
-              'rgba(54, 162, 235, 1)',
-            ],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          plugins: {
-            legend: {
-              position: 'top',
-              display: true,
-              labels: {
-                color: '#ffffff',
+      this.api.getData().subscribe({
+        next: (res) => {
+          console.log('produitsParMagasin API chart response:', res);
+          console.log('nbProduitsFabriques:', res.nbProduitsFabriques);
+          const labels = ['Pourcentage de production', 'Pourcentage de produits en vente'];
+          const data = [(res.pourcentageVendus / 100) * res.nbProduitsFabriques, res.nbProduitsFabriques];
+          new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+              labels: labels,
+              datasets: [{
+                data: data,
+                backgroundColor: [
+                  'rgba(75, 192, 192, 0.5)',
+                  'rgba(54, 162, 235, 0.5)',
+                ],
+                borderColor: [
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(54, 162, 235, 1)',
+                ],
+                borderWidth: 1
+              }]
+            },
+            options: {
+              plugins: {
+                legend: {
+                  position: 'top',
+                  display: true,
+                  labels: {
+                    color: '#ffffff',
+                  }
+                }
               }
             }
-          }
+          });
+        },
+        error: (err) => {
+          console.error('Erreur API chart:', err);
         }
       });
     }
--- src/app/services/api-service.ts
+++ src/app/services/api-service.ts
@@ -26,7 +26,7 @@ export interface ChartResponse {
   };
   nbProduitsFabriques: number;
   nbAccordVente: number;
-  pourcentageVentesProduits: number;
+  pourcentageVendus: number;
   nbProduitsMagasins: number;
   nbVentes: number;
   topFabricants: {
